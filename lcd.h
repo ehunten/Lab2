981b83d6705121ec6b7befe42c065f4e7fb1511e
ehunten <e.hunten@gmail.com>
2016-02-25 15:13:32 -0700

    Working project with working LCD

---
+++ lcd.h
@@ -0,0 +1,16 @@
+/*
+ * File:   lcd.h
+ * Authors: Evelyn Hunten
+ *
+ * Created 2/16/16
+ */
+
+void writeLCD(unsigned char word, unsigned int commandType, unsigned int delay); //done?
+void writeFourBits(unsigned char word, unsigned int commandType, unsigned int delayAfter, unsigned int lower); //done?
+void getTimeString ();
+void initLCD(void); //done?
+void clearLCD(void); //done?
+void moveCursorLCD(); //done?
+void printCharLCD(char c); //Done?
+void printStringLCD(const char* s); //in progress
+void testLCD(); //provided
